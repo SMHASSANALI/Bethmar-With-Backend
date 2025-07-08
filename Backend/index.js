533a5b19f741a70b41b2eedaf63b2ecc473ee071
SMHASSANALI <smhassanali@gmail.com>
2025-07-08 18:52:10 +0500

    Index.j

--- Backend/index.js
+++ Backend/index.js
@@ -1,40 +1,44 @@
-const dotenv = require("dotenv");
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
+const dotenv = require('dotenv')
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
 
-const AuthRoute = require("./routes/Authentication.route.js");
-const JobPostingRoute = require("./routes/JobPosting.route.js");
-const homePageRoute = require("./routes/HeroSection.route.js");
-const blogPostingRoute = require("./routes/BlogPosting.route.js");
+const AuthRoute = require('./routes/Authentication.route.js')
+const JobPostingRoute = require('./routes/JobPosting.route.js')
+const homePageRoute = require('./routes/HeroSection.route.js')
+const blogPostingRoute = require('./routes/BlogPosting.route.js')
 
-const connectToMongoDB = require("./db/db.js");
+const connectToMongoDB = require('./db/db.js')
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cookieParser());
+app.use(express.json())
+app.use(cookieParser())
 app.use(
-    cors({
-        origin: `${process.env.CLIENT_URL}`,
-        credentials: true,
-    })
-);
+  cors({
+    origin: `${process.env.CLIENT_URL}`,
+    credentials: true
+  })
+)
 
-app.use("/api/auth", AuthRoute);
-app.use("/api/job-posting", JobPostingRoute);
-app.use("/api/blog-posting", blogPostingRoute);
-app.use("/api/home-page", homePageRoute);
+app.use('/api/auth', AuthRoute)
+app.use('/api/job-posting', JobPostingRoute)
+app.use('/api/blog-posting', blogPostingRoute)
+app.use('/api/home-page', homePageRoute)
 
-// app.use("*", (req, res) => {
-//     res.status(404).json({ message: "Route not found" });
-// });
+app.get('/', (req, res) => {
+  res.send('Welcome to the API!')
+})
 
-const PORT = process.env.PORT || 8000;
+// app.use('*', (req, res) => {
+//   res.status(404).json({ message: 'Route not found' })
+// })
+
+const PORT = process.env.PORT || 8000
 
 app.listen(process.env.PORT || PORT, () => {
-    console.log(`Server is running on port ${PORT || 8000}`);
-    connectToMongoDB();
-});
+  console.log(`Server is running on port ${PORT || 8000}`)
+  connectToMongoDB()
+})
