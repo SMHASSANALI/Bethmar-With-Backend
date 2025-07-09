4f32c6de96faaa5865da305207fa9d30df55f54e
SMHASSANALI <smhassanali@gmail.com>
2025-07-09 18:48:11 +0500

    FInall

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -1,6 +1,5 @@
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
-console.log('ENV VARIABLE', process.env.NODE_ENV)
 const generateTokenAndSetCookie = (userId, res) => {
   const token = jwt.sign({ id: userId.toString() }, process.env.JWT_SECRET, {
     expiresIn: '15d'
