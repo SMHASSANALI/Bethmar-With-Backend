ea49019aaf1791348958caca1aed3f758f011858
SMHASSANALI <smhassanali@gmail.com>
2025-07-09 18:34:22 +0500

    Trouble

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -1,13 +1,15 @@
-const jwt = require('jsonwebtoken');
-
+const jwt = require('jsonwebtoken')
+console.log('ENV VARIABLE', process.env.NODE_ENV)
 const generateTokenAndSetCookie = (userId, res) => {
-    const token = jwt.sign({ id: userId.toString() }, process.env.JWT_SECRET, { expiresIn: '15d' });
-    res.cookie("jwt", token, {
-        maxAge: 15 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
-        sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Strict',
-        secure: process.env.NODE_ENV === 'production'
-    });
-};
+  const token = jwt.sign({ id: userId.toString() }, process.env.JWT_SECRET, {
+    expiresIn: '15d'
+  })
+  res.cookie('jwt', token, {
+    maxAge: 15 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Strict',
+    secure: process.env.NODE_ENV === 'production'
+  })
+}
 
-module.exports = generateTokenAndSetCookie;
+module.exports = generateTokenAndSetCookie
