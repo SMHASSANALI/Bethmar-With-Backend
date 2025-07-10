392d6fd1a9bb28fdecf3e5022d23712e5de81de2
SMHASSANALI <smhassanali@gmail.com>
2025-07-10 15:22:24 +0500

    Changes

--- Frontend/src/pages/Blog.jsx
+++ Frontend/src/pages/Blog.jsx
@@ -1,55 +1,64 @@
-import React, { useState, useEffect, useContext } from "react";
-import Blogs from "../components/Blogs/Blogs";
-import { useNavigate } from "react-router-dom";
-import GetContext from "../context/Custom Get Context/Get.Context";
+import React, { useState, useEffect, useContext } from 'react'
+import Blogs from '../components/Blogs/Blogs'
+import { useNavigate } from 'react-router-dom'
+import GetContext from '../context/Custom Get Context/Get.Context'
 
 const Blog = () => {
-  const [blogData, setBlogData] = useState([]);
+  const [blogData, setBlogData] = useState([])
+  const [loading, setLoading] = useState(false)
 
-  const getContext = useContext(GetContext);
-  const { getDataFromAPI } = getContext;
+  const getContext = useContext(GetContext)
+  const { getDataFromAPI } = getContext
 
   const getData = async () => {
-    const data = await getDataFromAPI("blog-posting/get-active-blogs");
-    setBlogData(data);
-  };
+    setLoading(true)
+    const data = await getDataFromAPI('blog-posting/get-active-blogs')
+    setBlogData(data)
+    setLoading(false)
+  }
 
   useEffect(() => {
-    getData();
-  }, []);
+    getData()
+  }, [])
 
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
-  const handleBlogClick = (blogData) => {
-    navigate(`/blog/${blogData._id}`, { state: blogData });
-  };
+  const handleBlogClick = blogData => {
+    navigate(`/blog/${blogData._id}`, { state: blogData })
+  }
 
   return (
-    <div className="bg-[#f9f9f9] min-h-screen">
-      <div className="relative">
-        <div className="max-w-[1500px] mx-auto gap-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 px-4 py-8">
-          {blogData &&
-            blogData.length > 0 &&
-            blogData.map((blog, index) => {
-              return (
-                <Blogs
-                  key={index}
-                  title={blog.title}
-                  date={blog.createdAt}
-                  description={blog.description}
-                  image={blog.image}
-                  category={blog.category}
-                  author={blog.author}
-                  content={blog.content}
-                  onClick={() => handleBlogClick(blog)}
-                />
-              );
-            })}
-        </div>
+    <div className='bg-[#f9f9f9] min-h-screen'>
+      <div className='relative'>
+        {loading ? (
+          <div className='w-ful'>
+            <div className='mx-auto animate-spin h-12 w-12 border-2 border-accentGreen border-t-transparent rounded-full' />
+          </div>
+        ) : (
+          <div className='max-w-[1500px] mx-auto gap-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 px-4 py-8'>
+            {blogData &&
+              blogData.length > 0 &&
+              blogData.map((blog, index) => {
+                return (
+                  <Blogs
+                    key={index}
+                    title={blog.title}
+                    date={blog.createdAt}
+                    description={blog.description}
+                    image={blog.image}
+                    category={blog.category}
+                    author={blog.author}
+                    content={blog.content}
+                    onClick={() => handleBlogClick(blog)}
+                  />
+                )
+              })}
+          </div>
+        )}
         {/* <Seprator color={'bg-moving-gradient'} /> */}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Blog;
+export default Blog
--- Frontend/src/pages/Careers.jsx
+++ Frontend/src/pages/Careers.jsx
@@ -1,82 +1,92 @@
-import React, { useContext, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import GetContext from "../context/Custom Get Context/Get.Context";
+import React, { useContext, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import GetContext from '../context/Custom Get Context/Get.Context'
 
 const Careers = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
-  const context = useContext(GetContext);
-  const { getDataFromAPI } = context;
+  const context = useContext(GetContext)
+  const { getDataFromAPI } = context
 
-  const [jobListings, setJobListings] = useState([]);
+  const [jobListings, setJobListings] = useState([])
+
+  const [loading, setLoading] = useState(false)
 
   const getData = async () => {
-    const data = await getDataFromAPI("job-posting/get-active-jobs");
-    setJobListings(data);
-  };
+    setLoading(true)
+    const data = await getDataFromAPI('job-posting/get-active-jobs')
+    setJobListings(data)
+    setLoading(false)
+  }
 
   useEffect(() => {
-    getData();
-  }, []);
+    getData()
+  }, [])
 
-  const onClick = (job) => {
-    navigate(`/careers/${job._id}`, { state: job });
-  };
+  const onClick = job => {
+    navigate(`/careers/${job._id}`, { state: job })
+  }
 
   return (
-    <main className="bg-[#f9f9f9] py-8">
-      <div className="max-w-[1500px] mx-auto rounded-lg shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] p-4 bg-white text-gray-800">
-        <div className="mb-8 py-[30px]">
-          <span className="text-sm bg-accentGreen px-3 py-1 rounded-full inline-block mb-1 text-white">
+    <main className='bg-[#f9f9f9] py-8'>
+      <div className='max-w-[1500px] mx-auto rounded-lg shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] p-4 bg-white text-gray-800'>
+        <div className='mb-8 py-[30px]'>
+          <span className='text-sm bg-accentGreen px-3 py-1 rounded-full inline-block mb-1 text-white'>
             We’re hiring!
           </span>
-          <h1 className="font-oswald text-2xl lg:text-4xl font-semibold tracking-wide leading-tight mb-4">
+          <h1 className='font-oswald text-2xl lg:text-4xl font-semibold tracking-wide leading-tight mb-4'>
             Be part of our mission
           </h1>
-          <p className="text-sm lg:text-base font-light font-Poppins text-gray-700 max-w-3xl">
+          <p className='text-sm lg:text-base font-light font-Poppins text-gray-700 max-w-3xl'>
             We're looking for passionate people to join us on our mission. We
             value flat hierarchies, clear communication, and full ownership and
             responsibility.
           </p>
         </div>
 
-        <div className="space-y-[30px]">
-          {jobListings.map((job, index) => (
-            <div
-              key={index}
-              className="py-6 px-2 border-y border-gray-300 flex flex-col md:flex-row justify-between items-start md:items-center transition"
-            >
-              <div>
-                <h2 className="text-xl font-semibold mb-1">{job.jobTitle}</h2>
-                <p className="text-gray-600 mb-2">{job.jobDescription}</p>
-                <div className="flex gap-2 text-sm">
-                  <span className="px-3 py-1 bg-gray-100 text-gray-600 rounded-full">
-                    {job.location}
-                  </span>
-
-                  <span className="px-3 py-1 bg-gray-100 text-gray-600 rounded-full">
-                    {job.jobType}
-                  </span>
-                </div>
-              </div>
-
-              <button
-                onClick={() => onClick(job)}
-                className="group mt-4 md:mt-0 flex items-center gap-2 text-primary font-medium hover:bg-primary hover:text-white pr-2 pl-4 py-2 rounded-full transition-all ease-in-out"
+        {loading ? (
+          <div className='w-ful'>
+            <div className='mx-auto animate-spin h-12 w-12 border-2 border-accentGreen border-t-transparent rounded-full' />
+          </div>
+        ) : (
+          <div className='space-y-[30px]'>
+            {jobListings.map((job, index) => (
+              <div
+                key={index}
+                className='py-6 px-2 border-y border-gray-300 flex flex-col md:flex-row justify-between items-start md:items-center transition'
               >
-                Apply
-                <img
-                  src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAACXBIWXMAAAsTAAALEwEAmpwYAAABbklEQVR4nO3asWoWQRQG0GuCMegzKOoLWKUVbIXUKURBQbSzECztAkmhIYVFaovUgog+iz6BGoiFon+CR5akEFEyxcrmn3sPbD/zsTsze+dGlFJKKaWUUkqZHs7hKhYj4cSf48CRfTyMLLDl755F77CE7/7tafQMF53sSfQKZ/CpIYRH0SvcbQjgJx5Er7DeGML96BU2G0I4xK3oeD140RjCWnQcwk5DCDOsRo+wgJcNIfzAzegRFrHbEMI33Ige4SxeNYTwFdej46Py64YQvmDlfw3iAu4c79UbEzzbx4veSfZwbezJX8EH8+PjMOYxA3hn/rwZa/LLja/eaTMUVpbHqtTMzJ/ZKAEM8FbWT2CAy3gv6yI4wHncTrkNTu1UHISmkvooLPPPkMy/wzIXRGQviclcFJW5LI57aS9GHH33n9NejeFS9svRpdTX44OhESJtg8QfLTKz31pkHkfiJqmFqcdSSimllFJKKdGrXwe8Dt5fSxhZAAAAAElFTkSuQmCC"
-                  className="group-hover:rotate-0 -rotate-45 transition-all ease-in-out duration-200 w-7 h-7 p-1 bg-[#26803d] rounded-full"
-                  alt=""
-                />
-              </button>
-            </div>
-          ))}
-        </div>
+                <div>
+                  <h2 className='text-xl font-semibold mb-1'>{job.jobTitle}</h2>
+                  <p className='text-gray-600 mb-2'>{job.jobDescription}</p>
+                  <div className='flex gap-2 text-sm'>
+                    <span className='px-3 py-1 bg-gray-100 text-gray-600 rounded-full'>
+                      {job.location}
+                    </span>
+
+                    <span className='px-3 py-1 bg-gray-100 text-gray-600 rounded-full'>
+                      {job.jobType}
+                    </span>
+                  </div>
+                </div>
+
+                <button
+                  onClick={() => onClick(job)}
+                  className='group mt-4 md:mt-0 flex items-center gap-2 text-primary font-medium hover:bg-primary hover:text-white pr-2 pl-4 py-2 rounded-full transition-all ease-in-out'
+                >
+                  Apply
+                  <img
+                    src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAACXBIWXMAAAsTAAALEwEAmpwYAAABbklEQVR4nO3asWoWQRQG0GuCMegzKOoLWKUVbIXUKURBQbSzECztAkmhIYVFaovUgog+iz6BGoiFon+CR5akEFEyxcrmn3sPbD/zsTsze+dGlFJKKaWUUkqZHs7hKhYj4cSf48CRfTyMLLDl755F77CE7/7tafQMF53sSfQKZ/CpIYRH0SvcbQjgJx5Er7DeGML96BU2G0I4xK3oeD140RjCWnQcwk5DCDOsRo+wgJcNIfzAzegRFrHbEMI33Ige4SxeNYTwFdej46Py64YQvmDlfw3iAu4c79UbEzzbx4veSfZwbezJX8EH8+PjMOYxA3hn/rwZa/LLja/eaTMUVpbHqtTMzJ/ZKAEM8FbWT2CAy3gv6yI4wHncTrkNTu1UHISmkvooLPPPkMy/wzIXRGQviclcFJW5LI57aS9GHH33n9NejeFS9svRpdTX44OhESJtg8QfLTKz31pkHkfiJqmFqcdSSimllFJKKdGrXwe8Dt5fSxhZAAAAAElFTkSuQmCC'
+                    className='group-hover:rotate-0 -rotate-45 transition-all ease-in-out duration-200 w-7 h-7 p-1 bg-[#26803d] rounded-full'
+                    alt=''
+                  />
+                </button>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </main>
-  );
-};
+  )
+}
 
-export default Careers;
+export default Careers
